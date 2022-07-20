784d9e3c668074c5f08c897d21e734d730d1ac75
kenne <kenne@DESKTOP-T7NKTC0>
2022-07-20 18:34:27 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "James St Patrick";
+            cookie.Expiry = new DateTime(1981, 05, 07);
+            System.Console.WriteLine(cookie["name"]);
+            System.Console.WriteLine(cookie.Expiry);
+        }
+    }
+}
